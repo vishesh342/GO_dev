345cafbfcbc4e9b4ab0645d9b80032d87d73e375
visheshkush342 <visheshkush342@gmail.com>
2024-07-02 13:08:16 -0700

    Generating simple server

--- Microservices/main.go
+++ Microservices/main.go
@@ -1,15 +1,53 @@
 package main
 
 import (
-	"net/http"
+	"context"
 	"log"
 	"microservice/handler"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 func main(){
 	logger := log.New(log.Writer(), "logger: ", log.LstdFlags)
 	sh := handler.NewServerLogger(logger)
-	sm := http.NewServeMux()
-	sm.Handle("/", sh)
+	ServMux := http.NewServeMux()
+	ServMux.Handle("/", sh)
 
-	http.ListenAndServe(":9090",sm)
+	// create a new server
+	s := http.Server{
+		Addr:         ":9090",           // configure the bind address
+		Handler:      ServMux,           // set the default handler
+		ErrorLog:     logger,            // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+	}
+
+	// start the server
+	go func() {
+		logger.Println("Starting server on port 9090")
+
+		err := s.ListenAndServe()
+		if err != nil {
+			logger.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
+
+	// http.ListenAndServe(":9090",sm)
 }
